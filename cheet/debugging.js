f16a7a292b13c8f8dfd4e7fb66e4588a09b5655e
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2022-02-11 21:17:04 +0900

    debugger

---
+++ cheet/debugging.js
@@ -0,0 +1,19 @@
+// Source remained
+// https://flaviocopes.com/debugging/
+
+// console.log();
+// console.dir();
+
+// JSON.stringify()
+// JSON.stringify(car, null, 2)
+
+const inspect = (obj) => {
+    for (const prop in obj) {
+      if (obj.hasOwnProperty(prop)) {
+        console.log(`${prop}: ${obj[prop]}`)
+      }
+    }
+  }
+  
+inspect(car)
+
