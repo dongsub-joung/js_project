7bb5b1edea3f1ec6b761367be904b74099468356
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2022-04-06 14:42:06 +0900

    chain data form validafy

--- blockChain/index.ts
+++ blockChain/index.ts
@@ -15,6 +15,14 @@ class Block{
             CryptoJS.SHA256(index + previsouHash + timestamp + data).toString();
         };
 
+    static validatedStructure= (aBlock: Block): boolean => {
+        typeof aBlock.index === "number" &&
+        typeof aBlock.hash === "string" &&
+        typeof aBlock.previsouHash === "string" &&
+        typeof aBlock.timestamp === "number" &&
+        typeof aBlock.data === "string" 
+    }    
+
     constructor(index: number,
         hash: string,
         previsouHash: string,
@@ -26,7 +34,7 @@ class Block{
             this.data= data;
             this.timestamp= timestamp;
         }
-}
+};
 
 const genesisBlock: Block= new Block(0, "20202020202", "", "hello", 123456);
 
@@ -41,5 +49,33 @@ const getLatesBlock= (): Block => {
 }
 
 const getNEwTimeStape= (): number => Math.round(new Date.getTime()/1000);
+
+const createNewBlock= (Date: string): Block => {
+    const previouseBlock: Blcok= getLatesBlock();
+    const newIndex: number= previouseBlock.index + 1;
+    const newTimeStamp: number= Block.getNEwTimeStape();
+    const newHash: String= Block.calculateBalckHash(
+        newIndex, previouseBlock.hash,
+        newTimeStamp, data,
+    );
+
+    const newBlock: Block= new Block(
+        newIndex, newHash,
+        previouseBlock.hash,
+        data, newTimeStamp
+    );
+
+    return newBlock;
+};
+
+const isBalckValid= (candidateBlock: Block, previouseBlock: Block): boolean{
+    if(Block.validatedStructure(candidateBlock)){
+        return false;
+    } else if (previouseBlock.index + 1 !== candidateBlock.index){
+        return false;
+    } else if( previouseBlock.hash !== candidateBlock.previsouHash){
+        return false;
+    } else if ()
+};
 
 export{};
