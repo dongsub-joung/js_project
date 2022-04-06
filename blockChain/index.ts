752bd7e72a74124eb18cb0e766349b346503c2c9
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2022-04-06 14:06:54 +0900

    sha256 hashing

--- blockChain/index.ts
+++ blockChain/index.ts
@@ -1,10 +1,20 @@
+import * as CryptoJS from "crypto-js";
+
 class Block{
     public index: number;
     public hash: string;
     public previsouHash: string;
     public data: string;
     public timestamp: number;
-    
+
+    static calculateBalckHash= (index: number,
+        hash: string,
+        previsouHash: string,
+        data: string,
+        timestamp: number) => {
+            CryptoJS.SHA256(index + previsouHash + timestamp + data).toString();
+        };
+
     constructor(index: number,
         hash: string,
         previsouHash: string,
@@ -19,6 +29,17 @@ class Block{
 }
 
 const genesisBlock: Block= new Block(0, "20202020202", "", "hello", 123456);
+
 let blockchain: [Block]= [genesisBlock];
+
+// @ERR void' nor 'any'
+const getBlockChain= () : Block[] => {
+    blockchain;
+}
+const getLatesBlock= (): Block => {
+    blockchain(blockchain.length-1);
+}
+
+const getNEwTimeStape= (): number => Math.round(new Date.getTime()/1000);
 
 export{};
